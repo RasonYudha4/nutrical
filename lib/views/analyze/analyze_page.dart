09682058fc50d4da0df98f4aee3473e13413fbe1
rasonyudha4 <rasonyudha921@gmail.com>
2025-04-29 14:05:18 +0700

    Adding the take an image and upload in the analyze page

--- lib/views/analyze/analyze_page.dart
+++ lib/views/analyze/analyze_page.dart
@@ -1,16 +1,147 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class AnalyzePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AnalyzePage extends StatefulWidget {
   const AnalyzePage({super.key});
+
+  @override
+  State<AnalyzePage> createState() => _AnalyzePageState();
+}
+
+class _AnalyzePageState extends State<AnalyzePage> {
+  File? _image;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _takePhoto() async {
+    try {
+      final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+      if (photo != null) {
+        setState(() {
+          _image = File(photo.path);
+        });
+      }
+    } catch (e) {
+      print('Error taking photo: $e');
+    }
+  }
+
+  Future<void> _pickImageFromGallery() async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        setState(() {
+          _image = File(image.path);
+        });
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: 90),
           Container(
-            decoration: BoxDecoration(color: Colors.amber),
-            child: Text("Analyze Page"),
+            decoration: BoxDecoration(color: Color(0xFF89AC46)),
+            height: 250,
+            child:
+                _image == null
+                    ? Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.camera_alt_rounded, size: 80),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Select an Image of Food",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    : Image.file(
+                      _image!,
+                      width: double.infinity,
+                      height: 250,
+                      fit: BoxFit.cover,
+                    ),
+          ),
+          SizedBox(height: 100),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: _takePhoto,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF89AC46),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 90,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.camera_alt_rounded, size: 40),
+                              SizedBox(width: 20),
+                              Text(
+                                "Take an Image",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                GestureDetector(
+                  onTap: _pickImageFromGallery,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF89AC46),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 90,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.image_outlined, size: 40),
+                              SizedBox(width: 20),
+                              Text(
+                                "Upload from Gallery",
+                                style: TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios_rounded),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
